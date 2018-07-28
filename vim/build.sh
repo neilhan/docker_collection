d766d6dbd0baafea2551cc2b3a6b89d4d4ddf350
Neil Han <neil_han@intuit.com>
2018-07-28 09:25:30 -0700

    works for me

--- vim/build.sh
+++ vim/build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+if [ ! -d ./work_dir/bundle/vundle ]; then
+    git clone https://github.com/gmarik/vundle.git ./work_dir/bundle/vundle
+    git clone https://github.com/Valloric/YouCompleteMe ./work_dir/bundle/YouCompleteMe
+fi
+
 docker build --rm -t d_vim .
--- vim/run.sh
+++ vim/run.sh
@@ -7,23 +7,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # --privileged  ### have the same effect, chrome, firefox can run now.
 docker run --rm -it \
     --name d_vim \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-e DISPLAY=unix$DISPLAY \
-	--device /dev/dri \
-	--device /dev/snd \
-	--device /dev/video0 \
-	--device /dev/input \
-	-v /etc/localtime:/etc/localtime:ro \
-	-v /etc/hosts:/etc/hosts:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    --device /dev/dri \
+    --device /dev/snd \
+    --device /dev/video0 \
+    --device /dev/input \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /etc/hosts:/etc/hosts:ro \
     -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
-	--net=host \
+    -v /dev/shm:/dev/shm \
     -u=$UID:$(id -g $USER) \
-	--volume="/dev/shm:/dev/shm" \
+    --privileged \
+    --workdir="/home/gui" \
     --volume=$HOME/:/home/gui \
     --volume=$DIR/work_dir:/home/gui/.vim \
-	--privileged \
-	d_vim vim $@
+    d_vim vim $@
 
 # --user=$USER \
 # --workdir="/home/$USER" \
 # --volume="$PWD/home:/home" \
+# --net=host \
