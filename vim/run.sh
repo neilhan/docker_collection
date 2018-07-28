a16cf4e5f6d36df04b8edd8b5d7bdffa2a40ca52
Neil Han <neil_han@intuit.com>
2018-07-28 09:46:40 -0700

    timezone

--- vim/run.sh
+++ vim/run.sh
@@ -2,29 +2,44 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# for volumes, please keep /dev/shm. the rest is your choice. 
-# --cap-add=SYS_ADMIN \ ### this gets chrome and firefox working
-# --privileged  ### have the same effect, chrome, firefox can run now.
-docker run --rm -it \
-    --name d_vim \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e DISPLAY=unix$DISPLAY \
-    --device /dev/dri \
-    --device /dev/snd \
-    --device /dev/video0 \
-    --device /dev/input \
-    -v /etc/localtime:/etc/localtime:ro \
-    -v /etc/hosts:/etc/hosts:ro \
-    -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
-    -v /dev/shm:/dev/shm \
-    -u=$UID:$(id -g $USER) \
-    --privileged \
-    --workdir="/home/gui" \
-    --volume=$HOME/:/home/gui \
-    --volume=$DIR/work_dir:/home/gui/.vim \
-    d_vim vim $@
+if [ "$(uname)" == "Darwin" ]; then
+    docker run --rm -it \
+        --name d_vim \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=unix$DISPLAY \
+        --device /dev/dri \
+        --device /dev/snd \
+        --device /dev/video0 \
+        --device /dev/input \
+        -v $DIR/timezone:/etc/timezone \
+        -v /etc/hosts:/etc/hosts:ro \
+        -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
+        -v /dev/shm:/dev/shm \
+        -u=$UID:$(id -g $USER) \
+        --privileged \
+        --workdir="/home/gui" \
+        --volume=$HOME/:/home/gui \
+        --volume=$DIR/work_dir:/home/gui/.vim \
+        d_vim vim $@
 
-# --user=$USER \
-# --workdir="/home/$USER" \
-# --volume="$PWD/home:/home" \
-# --net=host \
+else
+# elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    docker run --rm -it \
+        --name d_vim \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -e DISPLAY=unix$DISPLAY \
+        --device /dev/dri \
+        --device /dev/snd \
+        --device /dev/video0 \
+        --device /dev/input \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /etc/hosts:/etc/hosts:ro \
+        -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro \
+        -v /dev/shm:/dev/shm \
+        -u=$UID:$(id -g $USER) \
+        --privileged \
+        --workdir="/home/gui" \
+        --volume=$HOME/:/home/gui \
+        --volume=$DIR/work_dir:/home/gui/.vim \
+        d_vim vim $@
+fi
