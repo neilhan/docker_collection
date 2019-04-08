3d3376794b50bc84c4f77d2d03a25dc40b923d5d
Neil Han <neil_han@intuit.com>
2019-04-07 23:10:24 -0700

    :cat: emacs idea etc

--- serverless/after_build.sh
+++ serverless/after_build.sh
@@ -5,13 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 mkdir -p container/home/bin
 mkdir -p container/home/Projects
+
+# checkout spacemacs
+git clone https://github.com/syl20bnr/spacemacs container/home/.emacs.d
+
 cd $DIR/container/home/
 
 # intellij
-wget -c https://download.jetbrains.com/idea/ideaIU-2019.1.tar.gz idea.tar.gz
+wget -c https://download.jetbrains.com/idea/ideaIU-2019.1.tar.gz -O idea.tar.gz
 tar -xvf idea.tar.gz
 mv ./idea-IU-* bin/
 cd bin
+idea_dir=$(ls -d idea-IU-*)
+ln -s $idea_dir idea
  
 # # webstorm
 # wget -c https://download.jetbrains.com/webstorm/WebStorm-2019.1.tar.gz 
@@ -19,8 +25,5 @@ cd bin
 # mv WebStorm-191.6183.63 bin/
 # cd bin
 # ln -s WebStorm-191.6183.63 webstorm
-
-# checkout spacemacs
-git clone https://github.com/syl20bnr/spacemacs container/home/.emacs.d
 
 cd $DIR
