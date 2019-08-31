807182d6faf4176fcb9ef08fea99d0c0f20ba864
Neil Han <neil_han@intuit.com>
2019-08-30 22:48:34 -0700

    update gui build script

--- gui/build_new.sh
+++ gui/build_new.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# docker build --no-cache -t gui .
-docker build -t gui .
+docker build --no-cache -t gui .
+# docker build -t gui .
 # docker tag gui cgineilhan/gui
 # docker push cgineilhan/gui:latest
