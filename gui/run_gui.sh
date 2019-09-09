cff0cf2bb8a085699687dbbc24c0492078c45709
Neil Han <neil_han@intuit.com>
2019-09-08 23:49:15 -0700

    cleaning up some unnecessary stuff

--- gui/run_gui.sh
+++ gui/run_gui.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-# for volumes, please keep /dev/shm. the rest is your choice. 
+# for volumes, please keep /dev/shm. the rest is your choice.
 # --cap-add=SYS_ADMIN \ ### this gets chrome and firefox working
 # --privileged  ### have the same effect, chrome, firefox can run now.
 docker run --rm -it \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-e DISPLAY=unix$DISPLAY \
-	--device /dev/dri \
-	--device /dev/snd \
-	--device /dev/video0 \
-	--device /dev/input \
-	-v /etc/localtime:/etc/localtime:ro \
-	-v /etc/hosts:/etc/hosts:ro \
-	--net=host \
-        --user=$USER \
-	--volume="/dev/shm:/dev/shm" \
-        --volume="$PWD/home:/home" \
-        --volume="/home/nhan2/bin:/home/nhan2/bin" \
-        --volume="/home/nhan2/Downloads:/home/nhan2/Downloads" \
-        --volume="/home/nhan2/Projects:/home/nhan2/Projects" \
-        --volume="/home/nhan2/.m2:/home/nhan2/.m2" \
-	--volume="/home/nhan2/.IdeaIC2017.3:/home/nhan2/.IdeaIC2017.3" \
-        --workdir="/home/$USER" \
-	--privileged \
-	d_gui2 # /usr/bin/zsh # /usr/bin/gnome-terminal # /usr/bin/firefox # chromium-browser
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    --device /dev/dri \
+    --device /dev/snd \
+    --device /dev/video0 \
+    --device /dev/input \
+    -v /etc/hosts:/etc/hosts:ro \
+    --net=host \
+    --volume="/dev/shm:/dev/shm" \
+    --volume="$PWD/home:/home" \
+    --volume="$HOME/bin:/home/nhan2/bin" \
+    --volume="$HOME/Projects:/home/nhan2/Projects" \
+    --workdir="/home/$USER" \
+    --privileged \
+    gui # /usr/bin/zsh # /usr/bin/gnome-terminal # /usr/bin/firefox # chromium-browser
 
 	# -v $HOME/.config/app:/root/.config/app \
 
---
+++ local/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+build_new=$1
+
+if [ "$build_new" = "new" ]; then
+    echo reset docker image
+    opt="--no-cache"
+fi
+
+docker build \
+       --build-arg USER_NAME=$USER \
+       --build-arg USER_ID=$(id -u) \
+       --build-arg GROUP_NAME=$USER \
+       --build-arg GROUP_ID=$(id -g) \
+       -t local $opt .
